63ddf9bbcf4cb33b455450011550325187ab3536
cisen03 <78010123+cisen03@users.noreply.github.com>
2021-03-02 02:20:13 -0600

    Add files via upload

--- practicaD3/mapaingreso.js
+++ practicaD3/mapaingreso.js
@@ -102,10 +102,12 @@ d3.csv('ingreso.csv').then((data) => {
   // El dominio para el escalador ordinal
   color.domain(d3.map(data, d => d.continent))
 
-  x.domain([d3.min(data, d => d.country),
-            d3.max(data, d => d.country)])
+  x.domain([d3.min(data, d => d.income),
+            d3.max(data, d => d.income)])
   y.domain([d3.min(data, d => d.population),
             d3.max(data, d => d.population)])
+  r.domain([d3.min(data, d => d.income),
+            d3.max(data, d => d.income)])
 
   // Ejes
   xAxis = d3.axisBottom(x)
@@ -198,7 +200,7 @@ function render(data) {
   p.enter()
     .append('circle')
       .attr('r', 0)
-      .attr('cx', d => x(d.country))
+      .attr('cx', d => x(d.income))
       .attr('cy', d => y(d.population))
       .attr('fill', '#005500')
       .attr('clip-path', 'url(#clip)')
@@ -206,8 +208,9 @@ function render(data) {
       .attr('fill-opacity', 0.75)
     .merge(p)
       .transition().duration(600)
-      .attr('cx', d => x(d.country))
+      .attr('cx', d => x(d.income))
       .attr('cy', d => y(d.population))
+      .attr('r', d => r(d.income))
       .attr('fill', d => color(d.continent))
 
   p.exit()
